7d01adb420a17a819be357e2c93580f028d400d8
Tim Dettmers <tim.dettmers@gmail.com>
2019-07-25 17:14:55 -0700

    Fixed seed for hashing.

--- mnist_cifar/main.py
+++ mnist_cifar/main.py
@@ -48,6 +48,7 @@ def setup_logger(args):
     args_copy.iters = 1
     args_copy.verbose = False
     args_copy.log_interval = 1
+    args_copy.seed = 0
 
     log_path = './logs/{0}_{1}_{2}.log'.format(args.model, args.density, hashlib.md5(str(args_copy).encode('utf-8')).hexdigest()[:8])
     print(log_path)
