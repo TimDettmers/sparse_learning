7b68ee3b02842f8e0339019f5e33f2958ff9a3a4
Tim Dettmers <tim.dettmers@gmail.com>
2019-07-28 11:08:35 -0700

    Added median and --all arg for printing all results.

--- mnist_cifar/get_results_from_logs.py
+++ mnist_cifar/get_results_from_logs.py
@@ -1,5 +1,12 @@
 import glob
 import numpy as np
+import argparse
+
+parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+parser.add_argument('--all', action='store_true', help='Displays individual final results.')
+
+args = parser.parse_args()
+
 
 for log_name in glob.iglob('./logs/*.log'):
     losses = []
@@ -27,10 +34,17 @@ for log_name in glob.iglob('./logs/*.log'):
     print('='*85)
     print('Test set results for log: {0}'.format(log_name))
     print('Arguments:\n{0}\n'.format(arg))
-    print('Accuracy. Mean: {0:.5f}, Standard Error: {1:.5f}, Sample size: {2}, 95% CI: ({3:.5f},{4:.5f})'.format(np.mean(accs), acc_se, len(accs),
-        np.mean(accs)-(1.96*acc_se), np.mean(accs)+(1.96*acc_se)))
-    print('Error.    Mean: {0:.5f}, Standard Error: {1:.5f}, Sample size: {2}, 95% CI: ({3:.5f},{4:.5f})'.format(1.0-np.mean(accs), acc_se, len(accs),
-        (1.0-np.mean(accs))-(1.96*acc_se), (1.0-np.mean(accs))+(1.96*acc_se)))
-    print('Loss.     Mean: {0:.5f}, Standard Error: {1:.5f}, Sample size: {2}, 95% CI: ({3:.5f},{4:.5f})'.format(np.mean(losses), loss_se, len(losses),
-        np.mean(losses)-(1.96*loss_se), np.mean(losses)+(1.96*loss_se)))
+    print('Accuracy. Median: {5:.5f}, Mean: {0:.5f}, Standard Error: {1:.5f}, Sample size: {2}, 95% CI: ({3:.5f},{4:.5f})'.format(np.mean(accs), acc_se, len(accs),
+        np.mean(accs)-(1.96*acc_se), np.mean(accs)+(1.96*acc_se), np.median(accs)))
+    print('Error.    Median: {5:.5f}, Mean: {0:.5f}, Standard Error: {1:.5f}, Sample size: {2}, 95% CI: ({3:.5f},{4:.5f})'.format(1.0-np.mean(accs), acc_se, len(accs),
+        (1.0-np.mean(accs))-(1.96*acc_se), (1.0-np.mean(accs))+(1.96*acc_se), 1.0-np.median(accs)))
+    print('Loss.     Median: {5:.5f}, Mean: {0:.5f}, Standard Error: {1:.5f}, Sample size: {2}, 95% CI: ({3:.5f},{4:.5f})'.format(np.mean(losses), loss_se, len(losses),
+        np.mean(losses)-(1.96*loss_se), np.mean(losses)+(1.96*loss_se), np.median(losses)))
     print('='*85)
+
+    if args.all:
+        print('Individual results:')
+        for loss, acc in zip(losses, accs):
+            err = 1.0-acc
+            print('Loss: {0:.5f}, Accuracy: {1:.5f}, Error: {2:.5f}'.format(loss, acc, err))
+
